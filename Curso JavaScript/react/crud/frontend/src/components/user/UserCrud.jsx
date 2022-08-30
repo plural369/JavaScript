81429e428655f799282d9e31e0e8bd43ea8e38be
plural369 <biel15_14@hotmail.com>
2022-08-30 17:58:59 -0300

    att

--- Curso JavaScript/react/crud/frontend/src/components/user/UserCrud.jsx
+++ Curso JavaScript/react/crud/frontend/src/components/user/UserCrud.jsx
@@ -19,9 +19,10 @@ export default class UserCrud extends Component{
 
     state = {...initialState}
 
-    componenteWillMount(){
-        axios(baseUrl).then(resp => 
-            this.setState({list: resp.data}))
+    componentWillMount(){
+        axios(baseUrl).then(resp => {
+            this.setState({list: resp.data})
+        })
     }
 
     clear(){
@@ -38,9 +39,9 @@ export default class UserCrud extends Component{
         })
     }
 
-    getUpdatedList(user){
+    getUpdatedList(user, add = true){
         const list = this.state.list.filter(u => u.id !== user.id)
-        list.unshift(user)
+        if (add) list.unshift(user)
         return list
     }
 
@@ -86,10 +87,59 @@ export default class UserCrud extends Component{
         )
     }
 
+    load(user){
+        this.setState({user})
+    }
+
+    remove(user){
+        axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+            const list = this.getUpdatedList(user, false)
+            this.setState({list})
+        })
+    }
+
+    renderTable(){
+        return(
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        )
+    }
+
+    renderRows(){
+        return this.state.list.map(user => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btm-warning" onClick={()=> this.load(user)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2" onClick={() => this.remove(user)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
     render(){
+        console.log(this.state.list)
         return(
             <Main {...headerProps}>
                 {this.renderForm()}
+                {this.renderTable()}
             </Main>
         )
     }
